21ef410403997bdb672c5ac852b8e5de9d51f056
Nabil Echaouch <nabil.echaouch@digitas.com>
2018-08-27 14:03:07 +0200

    Use powerlevel9k zsh theme

--- init.sh
+++ init.sh
@@ -60,6 +60,7 @@ git clone https://github.com/jonmosco/kube-ps1.git ~/.oh-my-zsh/custom/plugins/k
 git clone https://github.com/zsh-users/zsh-completions.git ~/.oh-my-zsh/custom/plugins/zsh-completions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-autosuggestions.git ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
+git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
 
 
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
